5dfa64f1eca551e16c7f81009b6bff7be607d05b
FarisArmoush <fariskarmoush@gmail.com>
2024-12-21 15:26:34 +0300

    created `User` model.

---
+++ Authentication/Models/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  Authentication
+//
+//  Created by Faris Armoush on 21/12/2024.
+//
+
+import Foundation
+
+struct User {
+    let uuid: UUID
+    let firstName: String
+    let lastName: String
+    let email: String
+}

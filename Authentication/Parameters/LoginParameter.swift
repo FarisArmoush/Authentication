8d3382a191425410022c50512c5833f94337e961
FarisArmoush <fariskarmoush@gmail.com>
2024-12-21 15:27:55 +0300

    Create LoginParameter.swift

---
+++ Authentication/Parameters/LoginParameter.swift
@@ -0,0 +1,13 @@
+//
+//  LoginParameter.swift
+//  Authentication
+//
+//  Created by Faris Armoush on 21/12/2024.
+//
+
+import Foundation
+
+struct LoginParameter {
+    let email: String
+    let password: String
+}

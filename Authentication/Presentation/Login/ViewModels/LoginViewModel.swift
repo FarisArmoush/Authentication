8b4844e183c00d0157839e6083db1bf4a169ccd6
FarisArmoush <fariskarmoush@gmail.com>
2024-12-21 15:33:01 +0300

    Created Presentation/Login modules files.

---
+++ Authentication/Presentation/Login/ViewModels/LoginViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  LoginViewModel.swift
+//  Authentication
+//
+//  Created by Faris Armoush on 21/12/2024.
+//
+
+class LoginViewModel { }
---
+++ Authentication/Presentation/Login/Views/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  Authentication
+//
+//  Created by Faris Armoush on 21/12/2024.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text("Bye, World")
+    }
+}
+
+#Preview {
+    LoginView()
+}

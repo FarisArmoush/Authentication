f0a9df636b51132a62b81736c00b46f3579099a2
FarisArmoush <fariskarmoush@gmail.com>
2024-12-21 15:15:17 +0300

    Initial Commit

---
+++ Authentication/AuthenticationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthenticationApp.swift
+//  Authentication
+//
+//  Created by Faris Armoush on 21/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct AuthenticationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AuthenticationTests/AuthenticationTests.swift
@@ -0,0 +1,17 @@
+//
+//  AuthenticationTests.swift
+//  AuthenticationTests
+//
+//  Created by Faris Armoush on 21/12/2024.
+//
+
+import Testing
+@testable import Authentication
+
+struct AuthenticationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
